fea40edb87e35f5973305e3ffa38dff97b9ca3a2
Soumya0803 <soumyachopra@gmail.com>
2022-01-07 12:50:59 +0530

    Time: 54 ms (21.14%), Space: 14.7 MB (23.64%) - LeetHub

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -2,33 +2,35 @@ class Solution:
     def search(self, nums: List[int], target: int) -> int:
         if(not nums):
             return -1
-        pivot_index = self.find_pivot(nums) 
-        result = self.bin_search(nums, target, 0, pivot_index-1)
-        if(result!=-1):
-            return result
+        pivot=self.find_pivot(nums)
+        output=self.binary_search(nums,0,pivot-1,target)
+        if output!= -1:
+            return output
         else:
-            return self.bin_search(nums, target, pivot_index, len(nums)-1)
-        
-    def find_pivot(self, arr):
-	
-        element_to_compare = arr[-1]
-        low = 0
-        high = len(arr)-1    
-        while(low <= high):
-            mid = (low+high)//2
-            if element_to_compare < arr[mid]:
-                low = mid+1
-            elif element_to_compare >= arr[mid]:
-                high = mid-1
-        return low    
+            return self.binary_search(nums,pivot,len(nums)-1,target)
 
-    def bin_search(self, arr, value, low, high):     
-        while(low <= high):
-            mid = (low+high)//2
-            if value<arr[mid]:
-                high = mid-1
-            elif value>arr[mid]:
-                low = mid+1
-            else:
+            
+    def find_pivot(self, nums):
+        low=0
+        high=len(nums)-1
+        last_element = nums[-1]
+        while(low<=high):
+            mid=(high+low)//2
+            if last_element<nums[mid]:
+                low=mid+1
+            elif last_element>=nums[mid]:
+                high=mid-1
+        return low
+        
+    def binary_search(self,nums,low,high,target):
+        while(low<=high):
+            mid=(low+high)//2
+            if nums[mid]==target:
                 return mid
+            if target>nums[mid]:
+                low=mid+1
+            else:
+                high=mid-1
         return -1
+                
+                    

f4e9d1211e4eda3aa421c93a4cc585622416f9d2
Soumya0803 <soumyachopra@gmail.com>
2024-03-24 11:01:35 +0530

    Time: 43 ms (43.95%), Space: 12.1 MB (45.76%) - LeetHub

---
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def permuteUnique(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+         
+        permutations = []
+        counter = Counter(nums)
+        def findAllPermutations(res):
+            if len(res) == len(nums):
+                permutations.append(res)
+                return 
+            
+            for key in counter:
+                if counter[key]:
+                    counter[key]-=1 # decrement visited key
+                    findAllPermutations(res + [key])    
+                    counter[key]+=1 # restore the state of visited key to find the next path
+                
+        findAllPermutations([])
+        return permutations
